e7aa1719ef9d020eb1c586a7ac8b5035c8ab7ddd
Dean <dean.ottewell@diamond.ac.uk>
2017-03-07 14:58:17 +0000

    Fixed name typo + added controller code for manageing the listeners

---
+++ org.dawnsci.plotting.tools/src/org/dawnsci/plotting/tools/finding/IPeakOpportunityListener.java
@@ -0,0 +1,22 @@
+package org.dawnsci.plotting.tools.finding;
+
+import java.util.EventListener;
+
+/**
+ * TODO: data pass for peaks
+ * TODO: region bounds might be better off as seperate
+ * @author Dean P. Ottewell
+ *
+ */
+public interface IPeakOpportunityListener extends EventListener {
+
+	//TODO: realyl whats the difference between adding and removing event... is this distiction important?
+	public void peaksAdded(PeakOpportunityEvent evt);
+	public void peaksRemoved(PeakOpportunityEvent evt);
+	
+	//TODO: primarily use the change event for now.
+	public void peaksChanged(PeakOpportunityEvent evt);
+	
+	
+	//public void regionBoundsAdjustment();
+}
