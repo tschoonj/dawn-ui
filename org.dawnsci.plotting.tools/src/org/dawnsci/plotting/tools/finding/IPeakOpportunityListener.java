deace81ac514518661be6aed848e99b800c19be5
Dean <dean.ottewell@diamond.ac.uk>
2017-03-07 16:01:57 +0000

    Configured jobs to activate trigger + noted where move job bounds configuration

--- org.dawnsci.plotting.tools/src/org/dawnsci/plotting/tools/finding/IPeakOpportunityListener.java
+++ org.dawnsci.plotting.tools/src/org/dawnsci/plotting/tools/finding/IPeakOpportunityListener.java
@@ -10,13 +10,13 @@ import java.util.EventListener;
  */
 public interface IPeakOpportunityListener extends EventListener {
 
-	//TODO: realyl whats the difference between adding and removing event... is this distiction important?
-	public void peaksAdded(PeakOpportunityEvent evt);
-	public void peaksRemoved(PeakOpportunityEvent evt);
-	
 	//TODO: primarily use the change event for now.
 	public void peaksChanged(PeakOpportunityEvent evt);
 	
+	//TODO: realyl whats the difference between adding and removing event... is this distiction important?
+//	public void peaksAdded(PeakOpportunityEvent evt);
+//  public void peaksRemoved(PeakOpportunityEvent evt);
 	
 	//public void regionBoundsAdjustment();
+	
 }
